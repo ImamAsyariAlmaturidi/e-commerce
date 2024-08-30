52fd7172903c9b875afa42398ab2e7a0fd27bdc8
ImamAsyariAlmaturidi <imamasyari700@gmail.com>
2024-08-31 02:33:47 +0700

    fix: erro deploy

--- e-commerce/src/app/api/product/[slug]/route.ts
+++ e-commerce/src/app/api/product/[slug]/route.ts
@@ -1,21 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import { searchProductBySlug } from "@/db/models/Product";
+
 export async function GET(request: NextRequest) {
   const slug = request.headers.get("slug");
+
+  if (!slug) {
+    return NextResponse.json(
+      { message: "Slug tidak disediakan", data: null },
+      { status: 400 }
+    );
+  }
+
   try {
-    if (!slug) {
-      return null;
-    }
     const product = await searchProductBySlug(slug);
 
-    return NextResponse.json({
-      message: "success",
-      data: product,
-    });
+    if (!product) {
+      return NextResponse.json(
+        { message: "Produk tidak ditemukan", data: null },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Sukses", data: product },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({
-      message: "failed",
-      data: [],
-    });
+    console.error("Terjadi kesalahan:", error);
+
+    return NextResponse.json(
+      { message: "Terjadi kesalahan server", data: null },
+      { status: 500 }
+    );
   }
 }
